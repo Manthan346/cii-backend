d2bbb8f3cb04755b28a1028db22eaef4aa17dd73
varunraut22106106 <varunraut007@gmail.com>
2026-07-18 11:58:03 +0530

    created instructor dashboard api version 1 -It returns total candidates and active batches -It also returns overview of all batches which instructor is assigned to

--- backend/src/interfaces/instructor-auth-interface.ts
+++ backend/src/interfaces/instructor-auth-interface.ts
@@ -3,7 +3,7 @@ import { Request } from "express";
 export interface InstructorAuthRequest extends Request {
 
     instructor?: {
-
+        
         instructor_id?: string,
         email?: string,
         role:"instructor"
