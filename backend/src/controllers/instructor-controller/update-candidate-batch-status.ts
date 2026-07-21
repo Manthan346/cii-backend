f6ddcf588a98258e34d680aae2989aa10ce910d8
varunraut22106106 <varunraut007@gmail.com>
2026-07-21 16:15:51 +0530

    -created endpoint for to show all candidates + filters in candidate management page of instructor section -created endpoint to change status of candidate in badge in instructor section -created endpoint to view profile of candidate in candidate management page of instructor section -added those endpoints in instructor routes

---
+++ backend/src/controllers/instructor-controller/update-candidate-batch-status.ts
@@ -0,0 +1,105 @@
+import { Response } from "express";
+import { ApiError } from "../../helpers/ApiError";
+import { asyncHandler } from "../../helpers/asyncHandler";
+import { InstructorAuthRequest } from "../../interfaces/instructor-auth-interface";
+import { prisma } from "../../lib/prisma";
+import {
+    batch_enrollment_status_type
+}
+from "../../generated/prisma/enums";
+import { ApiResponse } from "../../helpers/ApiResponse";
+
+export const updateCandidateBatchStatus = asyncHandler(
+    async(req:InstructorAuthRequest,res:Response)=>{
+        const{enrollment_id,enrollment_status} = req.body;
+        const enrollment = await prisma.batch_enrollment.findUnique({
+            where:{
+                enrollment_id
+            }
+        })
+
+        if(!enrollment){
+            throw new ApiError(
+                404,
+                "Enrollment Not Found"
+            )
+        }
+
+        const batch = await prisma.batch_details.findUnique({
+            where:{
+                batch_id:enrollment.batch_id
+            }
+        })
+
+        if(
+            batch?.instructor_id !==
+            req.instructor?.instructor_id
+        ){
+            throw new ApiError(
+                403,
+                "You don't have permission to update this candidate's status."
+            );
+        }
+
+        if(
+            batch?.b_status !==
+            "ACTIVE"
+        ){
+            throw new ApiError(
+                400,
+                "This batch is not active."
+            );
+
+        }
+
+        const validStatus = Object.values(
+            batch_enrollment_status_type
+        );
+
+        if(
+            !validStatus.includes(
+                enrollment_status as
+                batch_enrollment_status_type
+            )
+        ){
+            throw new ApiError(
+                400,
+                "Invalid enrollment status."
+            );
+        }
+
+        if(
+            enrollment.enrollment_status ===
+            enrollment_status
+        ){
+            throw new ApiError(
+                400,
+                `Candidate already has ${enrollment_status} status.`
+            ); 
+        }
+        const oldStatus = enrollment.enrollment_status;
+
+        const updatedEnrollment =await prisma.batch_enrollment.update({
+            where:{
+                enrollment_id
+            },
+            data:{
+                enrollment_status
+            }
+        });
+
+        return res.status(200).json(
+            new ApiResponse(
+                200,
+                {
+                    old_status:
+                    oldStatus,
+                    new_status:
+                    updatedEnrollment
+                    .enrollment_status
+                },
+                "Candidate status updated successfully."
+            )
+        );
+    }
+)
