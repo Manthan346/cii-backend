9e838d7c928e95bc8f2edab7a37903683aff9319
varunraut22106106 <varunraut007@gmail.com>
2026-08-12 14:44:03 +0530

    created an endpoint to fetch all upcoming job-fair and job-events for the mobilizer

--- backend/src/routes/mobilizer-route/mobilizer-route.ts
+++ backend/src/routes/mobilizer-route/mobilizer-route.ts
@@ -2,6 +2,8 @@ import { Router} from 'express';
 import { verifyMobilizerUsingAccessToken } from '../../middlewares/mobilizer-auth-middleware/mobilizer-auth-middleware';
 import { getAllEnquiry } from '../../controllers/mobilizer-controller/get-all-enquiry';
 import { paginationMiddleware } from '../../middlewares/pagination-middleware/pagination';
+import { getAllJobEvents } from '../../controllers/mobilizer-controller/get-all-jobEvents';
+import { validateBody } from '../../middlewares/zod-middleware/zod-body-validator';
 
 const mobilizerRouter = Router();
 
@@ -11,5 +13,6 @@ mobilizerRouter.get(
     paginationMiddleware,
     getAllEnquiry
 );
+mobilizerRouter.get("/job-fair",verifyMobilizerUsingAccessToken,paginationMiddleware,getAllJobEvents)
 
 export default mobilizerRouter
