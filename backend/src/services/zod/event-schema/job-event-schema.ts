8ac8f06e111079649b9f7116225453934562ca61
varunraut22106106 <varunraut007@gmail.com>
2026-08-13 10:23:57 +0530

    endpoint has been made to update details of job-fair and job-drive

--- backend/src/services/zod/event-schema/job-event-schema.ts
+++ backend/src/services/zod/event-schema/job-event-schema.ts
@@ -26,3 +26,40 @@ export const createJobEventSchema = z.object({
     .optional()
 });
 
+export const updateJobEventSchema = z.object({
+  event_type: z
+    .enum(["JOB_FAIR", "JOB_DRIVE"])
+    .optional(),
+
+  event_name: z
+    .string()
+    .min(1, "Event name is required")
+    .max(255)
+    .optional(),
+
+  event_date: z
+    .coerce
+    .date()
+    .optional(),
+
+  event_time: z
+    .string()
+    .min(1, "Event time is required")
+    .optional(),
+
+  address: z
+    .string()
+    .min(1, "Address is required")
+    .optional(),
+
+  google_map_link: z
+    .string()
+    .url("Invalid Google Maps URL")
+    .optional(),
+
+  description: z
+    .string()
+    .optional(),
+});
+
+
