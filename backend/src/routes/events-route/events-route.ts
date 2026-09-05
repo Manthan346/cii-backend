2c8d4cdb1dfb27c98ba9c63cef099bdb97551821
varunraut22106106 <varunraut007@gmail.com>
2026-09-05 10:32:59 +0530

    created endpoint to fetch all completed and upcoming job frrives/fairs

--- backend/src/routes/events-route/events-route.ts
+++ backend/src/routes/events-route/events-route.ts
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { getPublicEvents } from "../../controllers/event-controller/get-public-events";
 import { paginationMiddleware } from "../../middlewares/pagination-middleware/pagination";
+import { getPublicJobEvents } from "../../controllers/fetch-all-jobEvents";
 
 const eventsRouter = Router();
 
 // Public landing endpoint — no auth required
 eventsRouter.get("/public", paginationMiddleware, getPublicEvents);
+//Fetch all job fair/drive
+eventsRouter.get('/job-events',paginationMiddleware,getPublicJobEvents);
 
 // Public enquiry creation endpoint — no auth required (landing page form)
 
