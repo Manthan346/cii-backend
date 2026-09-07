382908d6ca14a567f67b5052d7336416bd6ef4da
jayesh rathod <jayeshrathod6616@gmail.com>
2026-09-07 16:32:19 +0530

    removed button

--- frontend/CII-ERP/src/components/adminpage/Operations/CourseManagement/CourseManagement/CourseManagement.jsx
+++ frontend/CII-ERP/src/components/adminpage/Operations/CourseManagement/CourseManagement/CourseManagement.jsx
@@ -122,9 +122,9 @@ const CourseManagement = () => {
             Manage course catalog, batches and trainers
           </p>
         </div>
-        <Button icon={FileDown} onClick={handleExport}>
+        {/* <Button icon={FileDown} onClick={handleExport}>
           Export As
-        </Button>
+        </Button> */}
       </div>
 
       <CourseManagementOverview stats={courseStats} />
